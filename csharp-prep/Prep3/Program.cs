162d54b8fb1a0ac53955e71bc7f1656d27763275
LRHOKO <hokopoko1@hotmail.com>
2025-01-23 14:07:15 -0600

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess_Number = 0;
+
+        while (guess_Number != number)
+        {
+            Console.WriteLine("What is the magic number?");
+            string guess = Console.ReadLine();
+            guess_Number = int.Parse(guess);
+
+            if (number > guess_Number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess_Number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else {
+                Console.WriteLine("That's correct!");
+            }
+        }
+
+
     }
 }
