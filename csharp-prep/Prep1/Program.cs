4b505b60ce43c80d4a3014a430c57e691ca4945c
LRHOKO <hokopoko1@hotmail.com>
2025-01-23 13:51:28 -0600

    Prep1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_Name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_Name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_Name}, {first_Name} {last_Name}.");
     }
 }
